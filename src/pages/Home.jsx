2904e2f4eb6ba2fcb32ca600c86dfe4162ec683c
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-25 10:34:46 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+export default function Home() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/about">About</a>
+                    <a href="/contacts">Contacts</a>
+                </nav>
+            </header>
+
+            <main>
+                <h1>Home Page</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum fugiat cumque quam eos mollitia, voluptas harum placeat aliquid fuga, aliquam non ipsam possimus corrupti quidem! Vel quas magni impedit omnis.</p>
+            </main>
+
+            <footer>
+                <p>copyright 2024</p>
+            </footer>
+        </>
+    )
+
+}
